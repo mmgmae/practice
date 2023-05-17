4769fdf3a279813e2b8364208bab46012ca5b2f8
mmgmae <grossman.mae@gmail.com>
2023-05-17 12:26:32 -0700

    complete if statements practice

---
+++ the_modern_js_bootcamp/u4_app.js
@@ -0,0 +1,25 @@
+// IF STATEMENTS
+// Example 1
+if (1 === 1) {
+    console.log("It's True!");
+}
+
+// Example 2
+let rating = 3;
+
+if (rating === 3) {
+    console.log('You are a superstar!');
+}
+
+// Example 3
+let num = 37;
+
+if (num % 2 !== 0) {
+    console.log('Odd number');
+}
+
+if (num % 2 === 0) {
+    console.log('Even number');
+}
+
+// Example 4
