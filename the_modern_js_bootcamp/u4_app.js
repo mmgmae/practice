e3ed83bc70cad361ef1f9177c21caa5eb2f13096
mmgmae <grossman.mae@gmail.com>
2023-05-17 12:31:56 -0700

    complete else if practice

--- the_modern_js_bootcamp/u4_app.js
+++ the_modern_js_bootcamp/u4_app.js
@@ -1,25 +1,29 @@
-// IF STATEMENTS
+// IF STATEMENTS + Else if Statements
 // Example 1
 if (1 === 1) {
     console.log("It's True!");
 }
 
 // Example 2
-let rating = 3;
+let rating = 1;
 
 if (rating === 3) {
     console.log('You are a superstar!');
 }
+else if (rating === 2) {
+    console.log('Meets expectations');
+}
+else if (rating === 1) {
+    console.log('Needs improvement');
+}
 
 // Example 3
-let num = 37;
+let num = 40;
 
 if (num % 2 !== 0) {
     console.log('Odd number');
 }
-
-if (num % 2 === 0) {
+else if (num % 2 === 0) {
     console.log('Even number');
 }
 
-// Example 4
