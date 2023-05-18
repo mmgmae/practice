c7c346449ce048ef705600673acbb511f3e6ce28
mmgmae <grossman.mae@gmail.com>
2023-05-18 14:10:09 -0700

    complete u4 exercises

--- the_modern_js_bootcamp/u4_app.js
+++ the_modern_js_bootcamp/u4_app.js
@@ -46,3 +46,128 @@ else {
 }
 
 console.log(`This is the current high score: ${highScore}`);
+
+
+// nesting conditionals
+
+let password = "hello kitty";
+
+if (password.length >= 6){
+    if(password.indexOf(' ') === -1){
+        console.log("Valid password");
+    }
+    else{
+        console.log("Password cannot include spaces.");
+    }
+}
+else {
+    console.log("Password must be longer");
+}
+
+//  truthy and falsy demo
+
+let mystery = "";
+
+if(mystery) {
+    console.log("TRUTHY");
+}
+else {
+    console.log("FALSY");
+}
+
+// LOGICAL OPERATORS
+//  && -- logical and
+
+let pass = "lemons";
+
+if(pass.length >= 6 && pass.indexOf(' ') === -1) {
+    console.log("Valid Password");
+}
+else {
+    console.log("Invalid Password");
+}
+
+
+let guess = 11;
+
+if (guess >= 1 && guess <= 10){
+    console.log("Number is between 1 and 10");
+}
+else {
+    console.log("Please guess a number between 1 and 10")
+}
+
+// || -- logical or
+
+let age = 30;
+
+if(age < 6 || age >= 65){
+    console.log("You get in for free!");
+}
+else(
+    console.log("$10 please!")
+)
+
+// let color = prompt('What color do you like?')
+let color = 'blue';
+
+if(color === 'purple' || color === 'lilac' || color === 'violet'){
+    console.log('Me too!');
+}
+else(
+    console.log('Oh... I think purple is the best color...')
+)
+
+
+// ! -- not operator
+
+let loggedInUser;
+
+if(!loggedInUser){
+    console.log('Please log in to view this page');
+}
+
+let flavor = "lemon";
+
+if(!(flavor === 'grape' || flavor === 'cherry')){
+    console.log("We don't have that flavor!")
+}
+
+
+// switch statement demo
+
+let day = 6;
+switch(day) {
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursday');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    case 6:
+    case 7:
+        console.log('It\'s the weekend!');
+        break;
+    default:
+        console.log('Invalid Weekday')
+}
+
+
+// ternary operator
+
+let luckyNum = 8;
+
+luckyNum === 7 ? console.log('Your number is lucky!') : console.log('Your number isn\'t very lucky...')
+
+
+let status = 'online';
+let statusColor = status === 'offline' ? 'red' : 'green';
