74a8655c1c2fbb5e4d5264ba9ec5debd8b6cba38
mmgmae <grossman.mae@gmail.com>
2023-05-30 10:36:14 -0700

    complete CHALLENGE 4 - Get Playing Card

--- the_modern_js_bootcamp/u8_challenge_app.js
+++ the_modern_js_bootcamp/u8_challenge_app.js
@@ -56,3 +56,40 @@ function isPangram(sentence) {
     return true;
 }
 
+
+// CHALLENGE 4 - Get Playing Card
+// Write a getCard() function which returns a random playing card object, like:
+// 		{
+// 			value: 'K'
+// 			suit: 'clubs'
+// 		}
+//Pick a random value from:
+//----2,3,4,5,6,7,8,9,10,J,Q,K,A
+//Pick a random suit from:
+//----clubs,spades, hearts, diamonds
+//Return both in an object
+
+// function getCard() {
+// const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+// const valIdx = Math.floor(Math.random() * values.length);
+// const value = values[valIdx];
+
+// const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+// const suitIdx = Math.floor(Math.random() * suits.length);
+// const suit = suits[suitIdx];
+// return {value: value, suit: suit};
+// }
+
+// refactor: 
+function pick(arr){
+    // returns random element from an array
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
+function getCard() {
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+
+return {value: pick(values), suit: pick(suits)};
+}
