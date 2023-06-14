583fa213d0f21c575c0cedeb1e14d8ed508d7ed0
mmgmae <grossman.mae@gmail.com>
2023-06-13 18:31:23 -0700

    complete 45

---
+++ one_week_python/u8_methods.py
@@ -0,0 +1,8 @@
+# Help and ipython
+
+# use help() function to give information
+help(str)
+
+# ipython lets you list methods for whatever you're working with if you click TAB after name_of_thing.TAB
+# ipython lets you use a ? instead of () to give information about a function
+
