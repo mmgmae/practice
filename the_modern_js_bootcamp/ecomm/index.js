0bcc697c2092fa203d191dd7b37137d355a53a34
mmgmae <grossman.mae@gmail.com>
2023-06-20 11:58:33 -0700

    implement authentication

--- the_modern_js_bootcamp/ecomm/index.js
+++ the_modern_js_bootcamp/ecomm/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
-const users = require('./repositories/users');
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+    keys: ['1lkasnd3nvaidafj0ngajsdhf89j']
+}));
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
     res.send(`
         <div>
+            Your id is : ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email" />
                 <input name="password" placeholder="password" />
@@ -22,7 +27,7 @@ app.get('/', (req, res) => {
 
 
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
     const { email, password, passwordConfirmation } = req.body;
 
     const existingUser = await usersRepo.getOneBy({ email });
@@ -34,8 +39,52 @@ app.post('/', async (req, res) => {
         return res.send('Passwords must match');
     }
 
+    const user = await usersRepo.create({ email, password });
+
+    req.session.userId = user.id;
+
     res.send('Account Created');
 });
+
+app.get('/signout', (req, res) => {
+    req.session = null;
+    res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <button>Sign In</button>
+            </form>
+        </div>
+    `)
+});
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) {
+        return res.send('Email not found');
+    }
+
+    const validPassword = await usersRepo.comparePasswords(
+        user.password,
+        password
+    );
+    if (!validPassword) {
+        return res.send('Invalid password');
+    }
+
+    req.session.userId = user.id;
+
+    res.send('You are signed in');
+})
+
 
 app.listen(3000, () => {
     console.log('listening...')
