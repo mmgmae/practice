7eeadf31f2b02337eb8abd3223e27ac9209f46a6
mmgmae <grossman.mae@gmail.com>
2023-06-30 19:44:56 -0700

    complete basic testing framework

--- the_modern_js_bootcamp/tme/index.js
+++ the_modern_js_bootcamp/tme/index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log('Running Test!!....');
+const Runner = require('./runner');
+const runner = new Runner();
+
+const run = async () => {
+    await runner.collectFiles(process.cwd());
+    runner.runTests();
+}
+
+run();
