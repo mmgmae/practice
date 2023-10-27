22c95d3c9d83cfffc71f2f59baf8adb3e8d479c4
mmgmae <grossman.mae@gmail.com>
2023-10-26 23:47:02 -0700

    Complete notes for first video of u9 - boolean basics

---
+++ one_week_python/u9_boolean.py
@@ -0,0 +1,17 @@
+# BOOLEANS
+# decision making -> broken down into true or false, yes or no decisions
+
+# True and False with exact casing and spelling is a boolean
+isAlive = true
+
+# COMPARISON OPERATORS
+
+# EQUALITY OPERATORS
+
+# COMPARING ACROSS TYPES
+
+# TRUTH AND FALSEYNESS
+
+# THE 'IN' OPERATOR
+
+# COMPARING STRINGS
